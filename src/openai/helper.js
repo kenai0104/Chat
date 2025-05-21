976bddb4bea14949d7e82a631d7cdad9a7f4d6b3
kalyaniannareddy6 <kalyanikenai@gmail.com>
2025-05-21 09:46:20 +0530

    helper file changes

--- src/openai/helper.js
+++ src/openai/helper.js
@@ -5,16 +5,35 @@ dotenv.config();
 const together = new Together();
 
 async function generateSQL(question) {
-  console.log("question", question);
+  const data = {
+    tables: ["products"],
+    columns: [
+      "Product_ID",
+      "Product_Name",
+      "Category",
+      "Stock_Qty",
+      "Unit_Price",
+      "Warehouse",
+      "Supplier_Name",
+      "Last_updated",
+    ],
+  };
+
+  const tableInfo = `Tables: ${data.tables.join(
+    ", "
+  )} | Columns: ${data.columns.join(", ")}`;
+
   const prompt = `You are a MySQL expert. Given a natural language question, write a syntactically correct SQL query. Only return the SQL code and nothing else.
 Question: ${question}
-SQL:`;
+This is the database structure: ${tableInfo}
+Write the correct SQL command:`;
+
+  console.log("Prompt:", prompt);
 
   const response = await together.chat.completions.create({
     model: "mistralai/Mistral-7B-Instruct-v0.1",
     messages: [{ role: "user", content: prompt }],
   });
-  console.log("question", question);
 
   return response.choices[0].message.content.trim();
 }
